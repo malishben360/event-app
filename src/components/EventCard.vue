cb011655e1e1d327875c484e696c4a0dc3f3bdeb
Elisha Benjamin <malishben360@gmail.com>
2022-11-01 11:49:41 +0100

    Single page components

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+
+  props: {
+    event: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventView from "../views/EventView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "event-view",
+    component: EventView,
   },
   {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    name: "about-view",
+    component: import("../views/AboutView.vue"),
   },
 ];
 
