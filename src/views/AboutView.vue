fa5e6c4bfcdc73c7eb7d4ee9a1ad07d521e4ed7b
Elisha Benjamin <malishben360@gmail.com>
2022-11-01 16:19:39 +0100

    Single page component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>Events to make the workd better</h1>
+    <h1>A site for events to better the world.</h1>
+    <router-link to="/">Events</router-link>
   </div>
 </template>
